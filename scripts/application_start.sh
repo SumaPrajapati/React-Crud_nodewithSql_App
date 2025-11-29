8dcf468d504415bd8cf12b6ff385878fcaf41416
Sumanlata Prajapati <sumap.dotnet@gmail.com>
2025-11-29 08:10:18 -0500

    Enhance application_start.sh for server setup
    Updated application start script to include server directory navigation and save PM2 process.

---
+++ scripts/application_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Navigate to the server directory
+cd /var/www/React-Crud_nodewithSql_App/server
+
+# Start the Node.js server using PM2
+pm2 start index.js --name "your-node-app"
+pm2 save
