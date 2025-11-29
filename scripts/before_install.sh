4e27d5951ae3ed61b5d349fabee0e660f21bb461
Sumanlata Prajapati <sumap.dotnet@gmail.com>
2025-11-29 15:13:16 -0500

    Update application_stop.sh for improved clarity

--- scripts/before_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Stop existing Node.js server if running
-pm2 stop all || true
-pm2 delete all || true
-
-# Clean up previous deployment artifacts if necessary
-rm -rf /var/React-Crud_nodewithSql_App/*
