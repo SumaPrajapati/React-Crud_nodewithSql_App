8313793e390d276642a323ee60f97f7a9747e989
Sumanlata Prajapati <sumap.dotnet@gmail.com>
2025-11-29 08:04:35 -0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,7 @@
-
 #!/bin/bash
-# Install Node.js and npm if not present
-# Install Nginx if not present
-sudo apt-get update && sudo apt-get install -y nginx
+# Stop existing Node.js server if running
+pm2 stop all || true
+pm2 delete all || true
+
+# Clean up previous deployment artifacts if necessary
+rm -rf /var/React-Crud_nodewithSql_App/*
