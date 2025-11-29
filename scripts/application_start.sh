305fe466c5d37998ac15e9974f700e24f6a6987c
Sumanlata Prajapati <sumap.dotnet@gmail.com>
2025-11-29 15:21:09 -0500

    Remove unnecessary comments and pm2 save command

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Navigate to the server directory
 cd /var/www/React-Crud_nodewithSql_App/server
 
 # Start the Node.js server using PM2
 /usr/bin/pm2 start index.js --name "your-node-app"
-pm2 save
