bca92d4e3da825885446273b5c24c605c8016bcf
Sumanlata Prajapati <sumap.dotnet@gmail.com>
2025-11-30 08:20:41 -0500

    Delete scripts directory

--- scripts/application_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /var/www/React-Crud_nodewithSql_App/server
-
-# Start the Node.js server using PM2
-/usr/bin/pm2 start index.js --name "your-node-app"
--- scripts/before_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DEPLOY_DIR="/var/www/React-Crud_nodewithSql_App"
-echo "Cleaning up deployment directory $DEPLOY_DIR..."
-rm -rf $DEPLOY_DIR/*
-# Note: The agent ensures the base directory exists before copying file
