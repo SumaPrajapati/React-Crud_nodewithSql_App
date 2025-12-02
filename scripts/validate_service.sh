65edf3659a792767db6de6fa36b4d321cdd1e054
Sumanlata Prajapati <sumap.dotnet@gmail.com>
2025-12-02 14:16:50 -0500

    Delete scripts directory

--- scripts/validate_service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Example validation: check if the app is listening on port 3000
-curl http://localhost:3000
-if [ $? -eq 0 ]; then
-  echo "Service is up and running."
-else
-  echo "Service validation failed."
-  exit 1
-fi
