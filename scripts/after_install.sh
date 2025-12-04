c197963f138e3de2076bf254909e8e6723172843
Sumanlata Prajapati <sumap.dotnet@gmail.com>
2025-12-04 10:30:07 -0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,9 +6,7 @@ APP_DIR="/var/www/React-Crud_nodewithSql_App"
 cd "$APP_DIR"
 
 echo "Installing server dependencies..."
-cd server
-npm install --production || npm install
-cd ..
+
 
 echo "after_install.sh completed successfully."
 
